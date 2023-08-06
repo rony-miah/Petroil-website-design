11a813d12ce2acb10bcced2703e3267ea347b42a
rony-miah <rony.mkt25@gmail.com>
2023-08-06 22:59:56 +0600

    tailwind css done

---
+++ js/script.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#F40404',
+        },
+        maxWidth: {
+          container: '1144px'
+        },
+        fontFamily: {
+          'poppins': ['Poppins', 'sans-serif'],
+          'montserrat': ['Montserrat', 'sans-serif'],
+          'roboto': ['Roboto', 'sans-serif'],
+          'opensans': ['Open Sans', 'sans-serif']
+        },
+        backgroundImage: {
+          'banner': "url('./images/banner.png')",
+          'slider1': "url('./images/slider1.png')",
+          'slider2': "url('./images/slider2.png')",
+          'slider3': "url('./images/slider3.png')",
+          'slider4': "url('./images/slider4.png')",
+          'service1': "url('./images/service1.png')",
+          'service2': "url('./images/service2.png')",
+          'service3': "url('./images/service3.png')",
+          'aboutimg': "url('./images/aboutimg.png')",
+          'blogimg1': "url('./images/blogimg1.png')",
+          'blogimg2': "url('./images/blogimg2.png')",
+          'blogimg3': "url('./images/blogimg3.png')"
+
+        }
+      }
+    }
+  }
